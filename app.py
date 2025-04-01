4318e7b3a7f58bea6ebd4ceece131206a803c34b
KiMaTsHaDoW <daviiiid.15@gmail.com>
2025-04-01 16:34:10 +0200

    Donde se añade

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
+from mysql.connector import errorcode
 from clases.constantes import *
-
 import mysql.connector
 
 
 class App:
     def __init__(self):
-        cnx = mysql.connector.connect(
-            user=USER,
-            password=PASSWORD,
-            host=HOST,
-            database=DATABASE
-        )
+        try:
+            self.cnx = mysql.connector.connect(
+                user=USER,
+                password=PASSWORD,
+                host=HOST,
+                database=DATABASE
+            )
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_BAD_DB_ERROR:
+                print('No existe la base de datos')
+            elif err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print('Error de conexion')
+            else:
+                print(err)
+        else:
+            self.cnx.autocommit = True
+
 
     def cargar_datos(self):
+        alumnos:list = []
+        libros:list = []
+        alumnos_cursos_libros: list = []
+        cursos: list = []
+        materias: list = []
+
+    def cargar_alumnos(self, alumnos):
+        cursor = self.cnx.cursor()
